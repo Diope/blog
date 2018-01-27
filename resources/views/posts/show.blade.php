2ffacbf1d44c94c628b163ff31872d5f7b7a153f
Dion Pettaway <dionp@fullsail.edu>
2018-01-27 16:56:17 -0500

    Still getting used to PHP syntax, added basic DB associations

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,6 +5,20 @@
       <h2>{{$post->title}}</h2>
       <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} by <a href="#">Mark</a></p>
       <p>{{$post->body}}</p>
+      <hr>
+
+      <div class="comments">
+        <ul class="list-group">
+          @foreach($post->comments as $comment)
+          <li class="list-group-item">
+            <strong>
+              {{$comment->created_at->diffForHumans()}}: &nbsp;
+            </strong>
+            {{$comment->body}}
+          </li>
+          @endforeach
+        </ul>
+      </div>
   </div>
 @endsection
 
