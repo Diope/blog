76d72800f72693b1d8c7cac6a408e1423c648f88
Dion Pettaway <dionp@fullsail.edu>
2018-01-27 00:04:23 -0500

    layouts and partials are exactly like rails

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,34 +1,29 @@
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" href="../../favicon.ico">
 
-    <title>Album example for Bootstrap</title>
+    <title>Blog Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
 
     <!-- Custom styles for this template -->
-    <link href="album.css" rel="stylesheet">
+    <link href="blog.css" rel="stylesheet">
   </head>
 
   <body>
 
-    
+    @include('partials/nav')
 
-      @include('partials/nav')
+      @yield('content')
 
-      
-        @yield('content')
-      
-
-      @include('partials/footer')
-
+    @include('partials/footer')
   </body>
 </html>
