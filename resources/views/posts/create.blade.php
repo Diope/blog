82be38c71f4e9d8aa1e7694dc1755a328146bb34
Dion Pettaway <dionp@fullsail.edu>
2018-01-27 16:31:13 -0500

    Laravel has a few scopes built in, neat

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,22 +5,26 @@
     <h1>Create Post</h1>
     <hr>
 
-  <form method="POST" action="/posts">
-    {{csrf_field()}}
+    
+    @include('partials.flash')
 
-  <div class="form-group">
-    <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" name="title">
-  </div>
+    <form method="POST" action="/posts">
+      {{csrf_field()}}
 
-  <div class="form-group">
-    <label for="body">Body</label>
-    <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Publist</button>
-</form>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" name="title">
+      </div>
 
-
-  </div>
+      <div class="form-group">
+        <label for="body">Body</label>
+        <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+      </div>
+      
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">Publish</button>
+      </div>
+      
+  </form>
+</div>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,10 @@
 @extends('partials/layout')
 
 @section('content')
-  <div class="col-sm-8 blog-main">
-    test
+  <div class="col-sm-8 blog-main">  
+      <h2>{{$post->title}}</h2>
+      <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}} by <a href="#">Mark</a></p>
+      <p>{{$post->body}}</p>
   </div>
 @endsection
 
